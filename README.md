fec259f3baf84f728b3ce4b48cca17b88a30ded4
Raghav Rastogi <raghavrastogi594@gmail.com>
2023-12-03 19:25:46 +0530

    add reference badge

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # go-combinators
 
+[![Go Reference](https://pkg.go.dev/badge/github.com/rag594/go-combinators.svg)](https://pkg.go.dev/github.com/rag594/go-combinators)
+
 # License
 [MIT](LICENSE)
--- combinators.go
+++ combinators.go
@@ -3,6 +3,21 @@ package combinators
 // Slice Container parametrised by Type T
 type Slice[T any] []T
 
+// Filter is a combinator which can be used in conjuction with other
+// combinators.
+// It is used to Filter out the elements based on the filterFunc
+// provided.
+// For example:
+//
+// intSlice := Slice[int]{1, 2, 3, 4, 5, 6}
+//
+//	evenPredicateFunc := func(n int) bool {
+//		return n%2 == 0
+//	}
+//
+// opEvenSlice := Slice[int]{2, 4, 6}
+// evenSlice := intSlice.Filter(evenPredicateFunc)
+// Output: Slice[int]{2, 4, 6}
 func (s Slice[T]) Filter(filterFunc func(T) bool) Slice[T] {
 	result := Slice[T]{}
 
